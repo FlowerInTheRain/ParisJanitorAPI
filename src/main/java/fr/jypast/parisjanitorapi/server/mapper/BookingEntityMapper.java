bd4c4990215f0d54594159a93c8f8afc4e166592
crichardd <crichard11@myges.fr>
2024-09-17 07:40:43 +0200

    creation de l'entity

---
+++ src/main/java/fr/jypast/parisjanitorapi/server/mapper/BookingEntityMapper.java
@@ -0,0 +1,28 @@
+package fr.jypast.parisjanitorapi.server.mapper;
+
+import fr.jypast.parisjanitorapi.domain.functionnal.model.booking.Booking;
+import fr.jypast.parisjanitorapi.server.entity.BookingEntity;
+
+public interface BookingEntityMapper {
+    static Booking toDomain(BookingEntity entity) {
+        return Booking.builder()
+                .id(entity.getId())
+                .propertyId(entity.getPropertyId())
+                .tenantId(entity.getTenantId())
+                .startDate(entity.getStartDate())
+                .endDate(entity.getEndDate())
+                .status(entity.getStatus())
+                .build();
+    }
+
+    static BookingEntity fromDomain(Booking domain) {
+        return BookingEntity.builder()
+                .id(domain.getId())
+                .propertyId(domain.getPropertyId())
+                .tenantId(domain.getTenantId())
+                .startDate(domain.getStartDate())
+                .endDate(domain.getEndDate())
+                .status(domain.getStatus())
+                .build();
+    }
+}
