b0c8a0e32b6004ee77b2a844ea3654a4ce444963
Sid-Athos <sa.bennaceur@gmail.com>
2024-09-23 22:17:31 +0200

    renaming paths in controlller

--- src/main/java/fr/jypast/parisjanitorapi/client/controller/FilesController.java
+++ src/main/java/fr/jypast/parisjanitorapi/client/controller/FilesController.java
@@ -1,6 +1,5 @@
 package fr.jypast.parisjanitorapi.client.controller;
 
-import com.azure.storage.blob.BlobContainerClient;
 import fr.jypast.parisjanitorapi.domain.functionnal.service.files.FilesManagement;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -10,7 +9,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -22,25 +20,29 @@ public class FilesController {
 	
 	@GetMapping("/pictures/property/list/{propertyReference}")
 	@ResponseStatus(OK)
-	public List<String> createContainer(@PathVariable String propertyReference){
+	public List<String> getUrlsForContainer(@PathVariable String propertyReference){
+		LOGGER.info("Retrieving property pictures URLs");
 		return filesManagement.getUrlsFromContainer(propertyReference);
 	}
 	
 	@DeleteMapping("/pictures/property/delete/{propertyReference}/{fileName}")
 	@ResponseStatus(OK)
 	public void deletePictureFromProperty(@PathVariable String propertyReference, @PathVariable String fileName){
+		LOGGER.info("Deleting property picture");
 		filesManagement.deleteFileFromContainer(propertyReference,fileName);
 	}
 	
-	@DeleteMapping("/pictures/profile/delete/{profileReference}/{fileName}")
+	@DeleteMapping("/pictures/profile/users/delete/{profileReference}/{fileName}")
 	@ResponseStatus(OK)
 	public void deletePictureFromProfile(@PathVariable String profileReference, @PathVariable String fileName){
+		LOGGER.info("Deleting user profile picture");
 		filesManagement.deleteFileFromContainer(profileReference,fileName);
 	}
 	
-	@PutMapping("/pictures/profile/update-picture/{profileReference}")
+	@PutMapping("/pictures/profile/users/update-picture/{profileReference}")
 	@ResponseStatus(OK)
 	public void updateProfilePic(@PathVariable String profileReference, @RequestParam("file") MultipartFile[] files	){
+		LOGGER.info("Updating user profile picture");
 		filesManagement.updateProfilePicture(files,profileReference);
 	}
 }
