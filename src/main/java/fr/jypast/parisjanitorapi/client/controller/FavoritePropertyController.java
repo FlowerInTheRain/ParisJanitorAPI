1031d38d0ebb9eddec1f1884ecadfa6407086333
crichardd <crichard11@myges.fr>
2024-09-27 21:40:15 +0200

    Mise en place des fav v3

--- src/main/java/fr/jypast/parisjanitorapi/client/controller/FavoritePropertyController.java
+++ src/main/java/fr/jypast/parisjanitorapi/client/controller/FavoritePropertyController.java
@@ -3,10 +3,13 @@ package fr.jypast.parisjanitorapi.client.controller;
 
 import fr.jypast.parisjanitorapi.client.service.AuthVerifierService;
 import fr.jypast.parisjanitorapi.domain.functionnal.model.property.FavoriteProperty;
+import fr.jypast.parisjanitorapi.domain.functionnal.model.user.User;
+import fr.jypast.parisjanitorapi.domain.functionnal.service.TokenControllerService;
 import fr.jypast.parisjanitorapi.domain.port.in.property.PropertyCreatorApi;
 import fr.jypast.parisjanitorapi.domain.port.in.property.PropertyDeleterApi;
 import fr.jypast.parisjanitorapi.domain.port.in.property.PropertyFinderApi;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,23 +26,37 @@ public class FavoritePropertyController {
     private final PropertyFinderApi propertyFinderApi;
     private final PropertyCreatorApi propertyCreatorApi;
     private final PropertyDeleterApi propertyDeleterApi;
+    private final TokenControllerService tokenControllerService;
+
 
     @PostMapping("/add")
     @ResponseStatus(HttpStatus.CREATED)
     public void addFavorite(
-            @RequestParam UUID userId, @RequestParam UUID propertyId) {
-        FavoriteProperty favorite = propertyCreatorApi.addFavorite(userId, propertyId);
+            @RequestHeader HttpHeaders headers,
+            @RequestParam UUID propertyId) {
+        UUID token = authVerifierService.getToken(headers);
+        User user = tokenControllerService.getUserByToken(token);
+        FavoriteProperty favorite = propertyCreatorApi.addFavorite(user.getId(), propertyId);
     }
 
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<List<FavoriteProperty>> getUserFavorites(@PathVariable UUID userId) {
-        List<FavoriteProperty> favorites = propertyFinderApi.getUserFavorites(userId);
+
+    @GetMapping("/user/me")
+    public ResponseEntity<List<FavoriteProperty>> getUserFavorites(@RequestHeader HttpHeaders headers) {
+        UUID token = authVerifierService.getToken(headers);
+        User user = tokenControllerService.getUserByToken(token);
+        List<FavoriteProperty> favorites = propertyFinderApi.getUserFavorites(user.getId());
         return ResponseEntity.ok(favorites);
     }
 
+
     @DeleteMapping("/remove")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void removeFavorite(@RequestParam UUID userId, @RequestParam UUID propertyId) {
-        propertyDeleterApi.removeFavorite(userId, propertyId);
+    public void removeFavorite(
+            @RequestHeader HttpHeaders headers,
+            @RequestParam UUID propertyId) {
+        UUID token = authVerifierService.getToken(headers);
+        User user = tokenControllerService.getUserByToken(token);
+        propertyDeleterApi.removeFavorite(user.getId(), propertyId);
     }
+
 }
